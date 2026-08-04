b58cdba4bc486449570aafabdf80eb559056434f
Dmytro Solop <dmtsolop@gmail.com>
2026-08-04 23:07:29 +0300

    Fix

--- main/main.c
+++ main/main.c
@@ -12,12 +12,12 @@ void app_main(void)
 #if CONFIG_HW_02_3_SUPERLOOP
     hw02_3_superloop_run();
 #elif CONFIG_HW_02_4_1_HW_INTERRUPTS
-    hw02_4_1_hw_interrupts_run();
+    hw02_4_1_run();
 #elif CONFIG_HW_02_4_2_HW_INTERRUPTS
-    hw02_4_2_hw_interrupts_run();
+    hw02_4_2_run();
 #elif CONFIG_HW_02_4_3_HW_INTERRUPTS
-    hw02_4_3_hw_interrupts_run();
+    hw02_4_3_run();
 #elif CONFIG_HW_02_4_4_HW_INTERRUPTS
-    hw02_4_4_hw_interrupts_run();
+    hw02_4_4_run();
 #endif
 }
