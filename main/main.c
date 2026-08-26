3de66ffee0a33677a65f196fb0e61c7453393b66
Dmytro Solop <dmtsolop@gmail.com>
2026-08-26 16:07:43 +0300

    Made entrance poin HW_03_1

--- main/main.c
+++ main/main.c
@@ -7,6 +7,7 @@
 #include "hw02_4_3_hw_interrupts.h"
 #include "hw02_4_4_hw_interrupts.h"
 #include "hw02_5_exhaust_fan.h"
+#include "hw03_1_adc_calib.h"
 #include "hw03_2_adc_ema.h"
 #include "traffic_fsm.h"
 
@@ -26,6 +27,8 @@ void app_main(void)
     exhaust_fan_init();
 #elif CONFIG_MINI_PROJ_TRAFFIC_LIGHT
     traffic_fsm_init();
+#elif CONFIG_HW_03_1_ADC_CALIB
+    hw03_1_run();
 #elif CONFIG_HW_03_2_ADC_EMA
     hw03_2_run();
 #endif
